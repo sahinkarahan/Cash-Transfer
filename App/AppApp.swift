ffcf517c94071a1da43512d119163367ad225576
sahinkarahan <sahin.karahan@hotmail.com>
2025-01-28 20:01:42 +0300

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Şahin Karahan on 28.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App
+//
+//  Created by Şahin Karahan on 28.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
